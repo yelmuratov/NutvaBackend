52d3c6e398f53e9fd4ee3f7f1cc045ee53901098
yelmuratov <yelimuratovsalimbay@gmail.com>
2025-06-30 18:53:49 +0500

    Migration

---
+++ NutvaCms.Persistence/Migrations/20250630135333_AddAgeToPurchaseRequest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NutvaCms.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAgeToPurchaseRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
